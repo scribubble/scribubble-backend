0cb6174042c2c5dcb35adec4a2d815fc13cdde8e
Hyejin Eom <hjeom.dev@gmail.com>
2022-02-22 03:12:51 +0900

    Fix: use mongoose using subdocument

--- db/schemas.js
+++ db/schemas.js
@@ -1,48 +1,46 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const vectorSchema = new Schema({
+const vector0Schema = new Schema({
   x: { type: Number, default: 0 },
   y: { type: Number, default: 0 },
   z: { type: Number, default: 0 },
 });
 
-const Vec = mongoose.model("Vec", vectorSchema);
+const vector1Schema = new Schema({
+    x: { type: Number, default: 1 },
+    y: { type: Number, default: 1 },
+    z: { type: Number, default: 1 },
+  });
 
 const lineSchema = new Schema({
   drawer_id: { type: String, default: '' },
-  linePositions: { type: [vectorSchema], default: [] },
+  linePositions: [ vector0Schema ],
   lineColor: { type: String, default: '#000000' },
   lineWidth: { type: Number, default: 0 },
   lineDashed: { type: Boolean, default: false },
   objName: { type: String, default: '' },
-  position: { type: vectorSchema, default: {}},
-  tfcPosition: { type: vectorSchema, default: {}},
-  tfcScale: { type: vectorSchema, default: {}},
-  tfcRotation: { type: vectorSchema, default: {}},
+  position: { type: vector0Schema, default: () => ({}) },
+  tfcPosition: { type: vector0Schema, default: () => ({}) },
+  tfcScale: { type: vector1Schema, default: () => ({}) },
+  tfcRotation: { type: vector0Schema, default: () => ({}) },
 });
-
-const Line = mongoose.model("Line", lineSchema);
 
 const shapeSchema = new Schema({
   objName: { type: String, default: '' },
   shape: { type: String, default: '' },
   color: { type: String, default: '#000000' },
-  position: { type: vectorSchema, default: {}},
-  scale: { type: vectorSchema, default: {}},
-  rotation: { type: vectorSchema, default: {}},
+  position: { type: vector0Schema, default: () => ({}) },
+  scale: { type: vector1Schema, default: () => ({}) },
+  rotation: { type: vector0Schema, default: () => ({}) },
 });
-
-const Shape = mongoose.model("Shape", shapeSchema);
 
 const bubbleSchema = new Schema({
   bubbleName: { type: String, default: '' },
   owner_id: { type: String, default: '' },
   visitor_id: [{ type: String, default: '' }],
-  lines: { type: [lineSchema], default: [] },
-  shapes: { type: [shapeSchema], default: [] },
+  lines: [ lineSchema ],
+  shapes: [ shapeSchema ],
 });
 
-const Bubble = mongoose.model("Bubble", bubbleSchema);
-
-module.exports = { Bubble, Line, Shape, Vec };
+module.exports = { bubbleSchema, lineSchema, shapeSchema, vector0Schema, vector1Schema };
