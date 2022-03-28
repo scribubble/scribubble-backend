c20ff6d2c56aa5c2033b29200f72cd5968009dfd
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-28 23:38:21 +0900

    Refactor: use .env variable when connect to db

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const { Bubble } = require("./models/Bubble");
 
-const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/scribubble";
+const DB_URL = process.env.DB_URL || "localhost";
+const DB_PORT = process.env.DB_PORT || "27017";
+const DB_COLLECTION = process.env.DB_COLLECTION || "scribubble";
+const DB_CONNECTIONSTRING = "mongodb://" + DB_URL + ":" + DB_PORT + "/" + DB_COLLECTION;
 
-mongoose.connect(DB_URL);
+mongoose.connect(DB_CONNECTIONSTRING);
+
 const db = mongoose.connection;
 
-db.on("connected", () => console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL));
-db.on("error", (error) => console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error));
+db.on("connected", () => console.log("DB is connected. " + DB_CONNECTIONSTRING));
+db.on("error", (error) => console.error("DB connection has failed.\n" + DB_CONNECTIONSTRING + "\n" + error));
 
 module.exports = { Bubble };
