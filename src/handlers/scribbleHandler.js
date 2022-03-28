70faaa1b6189a0491755c642fc33d9fdf2ab6a53
Hyejin Eom <hjeom.dev@gmail.com>
2022-03-28 22:02:39 +0900

    Fix: use try-catch & use function(not arrow function)

--- src/handlers/scribbleHandler.js
+++ src/handlers/scribbleHandler.js
@@ -26,119 +26,196 @@ function findObjByObjName(bubbleName, objName) {
 
 module.exports = (io) => {
   const handleDrawStart = function (payload) {
-    console.log(loadedData);
-    loadedData[payload.bubbleName].lines.push({
-      drawer_id: payload.user_id,
-      linePositions: [{ x: payload.mousePos.x, y: payload.mousePos.y, z: payload.mousePos.z}],
-      lineColor: payload.color,
-      lineWidth: payload.linewidth,
-      lineDashed: payload.dashed,
-      objName: payload.objName,
-      position: { x: 0, y: 0, z: 0,},
-      tfcPosition: { x: 0, y: 0, z: 0,},
-      tfcScale: { x: 1, y: 1, z: 1,},
-      tfcRotation: { x: 0, y: 0, z: 0,}
-    });
+    try {
+      console.log(loadedData);
+      loadedData[payload.bubbleName].lines.push({
+        drawer_id: payload.user_id,
+        linePositions: [
+          {
+            x: payload.mousePos.x,
+            y: payload.mousePos.y,
+            z: payload.mousePos.z,
+          },
+        ],
+        lineColor: payload.color,
+        lineWidth: payload.linewidth,
+        lineDashed: payload.dashed,
+        objName: payload.objName,
+        position: { x: 0, y: 0, z: 0 },
+        tfcPosition: { x: 0, y: 0, z: 0 },
+        tfcScale: { x: 1, y: 1, z: 1 },
+        tfcRotation: { x: 0, y: 0, z: 0 },
+      });
 
-    io.to(payload.bubbleName).emit("draw start", payload);
-  };
-  
-  const handleDrawing = async (payload) => {
-    let result = await findObjByObjName(payload.bubbleName, payload.objName);
-
-    loadedData[payload.bubbleName][result.objType][result.index].linePositions.push({
-      x: payload.mousePos.x,
-      y: payload.mousePos.y,
-      z: payload.mousePos.z,
-    });
-
-    io.to(payload.bubbleName).emit("drawing", payload);
-  };
-
-  const handleDrawStop = async (payload) => {
-    let result = await findObjByObjName(payload.bubbleName, payload.objName);
-
-    if(result.objType === 'lines') {
-      loadedData[payload.bubbleName][result.objType][result.index].tfcPosition = payload.tfcPosition;
-      loadedData[payload.bubbleName][result.objType][result.index].position = payload.position;
+      io.to(payload.bubbleName).emit("draw start", payload);
+    } catch (error) {
+      console.log(error);
     }
-    
-    io.to(payload.bubbleName).emit("draw stop", payload);
   };
 
-  const handleCreateShape = (payload) => {
-    const socket = this;
-    loadedData[payload.bubbleName].shapes.push({
-      shape: payload.shape,
-      color: payload.color,
-      objName: payload.objName,
-      position: { x: payload.position.x, y: payload.position.y, z: payload.position.z },
-      rotation: { x: payload.rotation.x, y: payload.rotation.y, z: payload.rotation.z },
-      scale: { x: payload.scale.x, y: payload.scale.y, z: payload.scale.z },
-    })
+  const handleDrawing = async function (payload) {
+    try {
+      let result = await findObjByObjName(payload.bubbleName, payload.objName);
 
-    socket.to(payload.bubbleName).emit("create shape", payload);
-  };
+      loadedData[payload.bubbleName][result.objType][
+        result.index
+      ].linePositions.push({
+        x: payload.mousePos.x,
+        y: payload.mousePos.y,
+        z: payload.mousePos.z,
+      });
 
-  const handleMvObj = async (payload) => {
-    const socket = this;
-    let result = await findObjByObjName(payload.bubbleName, payload.objName);
-
-    if(result.objType === 'lines' && payload.tfcPosition) {
-      loadedData[payload.bubbleName][result.objType][result.index].tfcPosition = payload.tfcPosition;
-    } else if(result.objType === 'shapes' && payload.position){
-      loadedData[payload.bubbleName][result.objType][result.index].position = payload.position;
+      io.to(payload.bubbleName).emit("drawing", payload);
+    } catch (error) {
+      console.log(error);
     }
-    
-    socket.to(payload.bubbleName).emit("move obj", payload);
   };
 
-  const handleRtObj = async (payload) => {
-    const socket = this;
-    const result = await findObjByObjName(payload.bubbleName, payload.objName);
-    
-    if(result.objType === 'lines') {
-      loadedData[payload.bubbleName][result.objType][result.index].tfcRotation = payload.rotation;
-    } else if(result.objType === 'shapes'){
-      loadedData[payload.bubbleName][result.objType][result.index].rotation = payload.rotation;
+  const handleDrawStop = async function (payload) {
+    try {
+      let result = await findObjByObjName(payload.bubbleName, payload.objName);
+
+      if (result.objType === "lines") {
+        loadedData[payload.bubbleName][result.objType][
+          result.index
+        ].tfcPosition = payload.tfcPosition;
+        loadedData[payload.bubbleName][result.objType][result.index].position =
+          payload.position;
+      }
+
+      io.to(payload.bubbleName).emit("draw stop", payload);
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(payload.bubbleName).emit("rotate obj", payload);
   };
 
-  const handleScObj = async (payload) => {
-    const socket = this;
-    const result = await findObjByObjName(payload.bubbleName, payload.objName);
-    
-    if(result.objType === 'lines') {
-      loadedData[payload.bubbleName][result.objType][result.index].tfcScale = payload.scale;
-    } else if(result.objType === 'shapes'){
-      loadedData[payload.bubbleName][result.objType][result.index].scale = payload.scale;
+  const handleCreateShape = function (payload) {
+    try {
+      const socket = this;
+      loadedData[payload.bubbleName].shapes.push({
+        shape: payload.shape,
+        color: payload.color,
+        objName: payload.objName,
+        position: {
+          x: payload.position.x,
+          y: payload.position.y,
+          z: payload.position.z,
+        },
+        rotation: {
+          x: payload.rotation.x,
+          y: payload.rotation.y,
+          z: payload.rotation.z,
+        },
+        scale: { x: payload.scale.x, y: payload.scale.y, z: payload.scale.z },
+      });
+
+      socket.to(payload.bubbleName).emit("create shape", payload);
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(payload.bubbleName).emit("scale obj", payload);
   };
 
-  const handleColorObj = async (payload) => {
-    const socket = this;
-    const result = await findObjByObjName(payload.bubbleName, payload.objName);
-    
-    if(result.objType === 'lines') {
-      loadedData[payload.bubbleName][result.objType][result.index].lineColor = payload.color;
-    } else if(result.objType === 'shapes'){
-      loadedData[payload.bubbleName][result.objType][result.index].color = payload.color;
+  const handleMvObj = async function (payload) {
+    try {
+      const socket = this;
+      let result = await findObjByObjName(payload.bubbleName, payload.objName);
+
+      if (result.objType === "lines" && payload.tfcPosition) {
+        loadedData[payload.bubbleName][result.objType][
+          result.index
+        ].tfcPosition = payload.tfcPosition;
+      } else if (result.objType === "shapes" && payload.position) {
+        loadedData[payload.bubbleName][result.objType][result.index].position =
+          payload.position;
+      }
+
+      socket.broadcast.emit("move obj", payload);
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(payload.bubbleName).emit("change obj color", payload);
   };
 
-  const handleDeleteObj = async (payload) => {
-    const socket = this;
-    const result = await findObjByObjName(payload.bubbleName, payload.objName);
-    
-    loadedData[payload.bubbleName][result.objType].splice(result.index, 1);
+  const handleRtObj = async function (payload) {
+    try {
+      const socket = this;
+      const result = await findObjByObjName(
+        payload.bubbleName,
+        payload.objName
+      );
 
-    socket.to(payload.bubbleName).emit("delete obj", payload);
+      if (result.objType === "lines") {
+        loadedData[payload.bubbleName][result.objType][
+          result.index
+        ].tfcRotation = payload.rotation;
+      } else if (result.objType === "shapes") {
+        loadedData[payload.bubbleName][result.objType][result.index].rotation =
+          payload.rotation;
+      }
+
+      socket.to(payload.bubbleName).emit("rotate obj", payload);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleScObj = async function (payload) {
+    try {
+      const socket = this;
+      const result = await findObjByObjName(
+        payload.bubbleName,
+        payload.objName
+      );
+
+      if (result.objType === "lines") {
+        loadedData[payload.bubbleName][result.objType][result.index].tfcScale =
+          payload.scale;
+      } else if (result.objType === "shapes") {
+        loadedData[payload.bubbleName][result.objType][result.index].scale =
+          payload.scale;
+      }
+
+      socket.to(payload.bubbleName).emit("scale obj", payload);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleColorObj = async function (payload) {
+    try {
+      const socket = this;
+      const result = await findObjByObjName(
+        payload.bubbleName,
+        payload.objName
+      );
+
+      if (result.objType === "lines") {
+        loadedData[payload.bubbleName][result.objType][result.index].lineColor =
+          payload.color;
+      } else if (result.objType === "shapes") {
+        loadedData[payload.bubbleName][result.objType][result.index].color =
+          payload.color;
+      }
+
+      socket.to(payload.bubbleName).emit("change obj color", payload);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeleteObj = async function (payload) {
+    try {
+      const socket = this;
+      const result = await findObjByObjName(
+        payload.bubbleName,
+        payload.objName
+      );
+
+      loadedData[payload.bubbleName][result.objType].splice(result.index, 1);
+
+      socket.to(payload.bubbleName).emit("delete obj", payload);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return {
